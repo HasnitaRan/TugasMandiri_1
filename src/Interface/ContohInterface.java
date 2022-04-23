5414c72e9d065860f5ec19162e3103651f6bd7e1
HasnitaRan <92636895+HasnitaRan@users.noreply.github.com>
2022-04-23 12:31:11 +0700

    Add files via upload

--- src/Interface/ContohInterface.java
+++ src/Interface/ContohInterface.java
@@ -1,8 +1,8 @@
 package Interface;
 
 public interface ContohInterface {
-    public String PrintJudul(String Judul);
-    public int HitungTambah(int a, int b);
-    public int HitungKali(int a, int b);
+    public void PrintJudul();
+    public void HitungTambah();
+    public void HitungKali();
 }
 
